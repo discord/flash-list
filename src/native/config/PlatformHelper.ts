7f87d60523cd1b820d983d5b2574f515fc6408e0
Daniel Friyia JR <daniel.friyia@shopify.com>
2023-02-10 09:17:15 -0500

    [DF] feat(#547): `experimentalMaintainTopContentPosition` works vertically
    [DF] feat(#547): `experimentalMaintainTopContentPosition` works horizontally
    
    [DF] feat(#547): `experimentalMaintainTopContentPosition` prop added to TypeScript
    
    [DF] feat(#547): update documentation
    
    [DF] feat(#547): final cleanups
    
    [DF] feat(#547): update podfile.lock
    
    [DF] feat(#547): `experimentalMaintainTopContentPosition` works vertically
    
    [DF] feat(#547): final cleanups
    
    [DF] feat(#547): `experimentalMaintainTopContentPosition` works vertically
    
    [DF] feat(#547): `experimentalMaintainTopContentPosition` works horizontally
    
    [DF] feat(#547): `experimentalMaintainTopContentPosition` prop added to TypeScript
    
    [DF] feat(#547): final cleanups
    
    [DF] feat(#547): retrieve diff value on android
    
    [DF] feat(#547): maintain android list position
    
    Fix rebase
    
    Fix Rebase
    
    Fix rebase
    
    [DF] feat(#547): clean up kotlin code
    
    [DF] feat(#547): cleanup typescript code
    
    [DF] feat(#547): rename some variables
    
    [DF] feat(#547): fix kotlin pr requests
    
    [DF] feat(#547): fix typescript pr requests

--- src/native/config/PlatformHelper.ts
+++ src/native/config/PlatformHelper.ts
@@ -1,6 +1,10 @@
+import { Platform, ScrollViewProps } from "react-native";
 import type * as React from "react";
-import type { BaseItemAnimator } from "recyclerlistview";
+import type { BaseItemAnimator, RecyclerListViewProps } from "recyclerlistview";
 import { DefaultJSItemAnimator } from "recyclerlistview/dist/reactnative/platform/reactnative/itemanimators/defaultjsanimator/DefaultJSItemAnimator";
+import React from "react";
+
+import { BidirectionalScrollView } from "../../BiDirectionalScrollView";
 
 const PlatformConfig = {
   defaultDrawDistance: 250,
@@ -22,9 +26,22 @@ const getFooterContainer = (): React.ComponentClass | undefined => {
   return undefined;
 };
 
+const getBidirectionalScrollView = (
+  experimentalScrollPositionManagement: boolean,
+  renderScrollComponent:
+    | React.FC<ScrollViewProps>
+    | React.ComponentType<ScrollViewProps>
+    | undefined
+) => {
+  return experimentalScrollPositionManagement && Platform.OS === "android"
+    ? (BidirectionalScrollView as unknown as RecyclerListViewProps["externalScrollView"])
+    : (renderScrollComponent as unknown as RecyclerListViewProps["externalScrollView"]);
+};
+
 export {
   PlatformConfig,
   getCellContainerPlatformStyles,
   getItemAnimator,
   getFooterContainer,
+  getBidirectionalScrollView,
 };
